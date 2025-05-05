f80b01d0b4c9e00730db4d5f98b2930b93c2f757
Akshay Kumar <akshaykumar@Akshays-MacBook-Air.local>
2025-05-05 10:27:39 +0530

    add starscream and swift protobuff in binary target

--- Package.swift
+++ Package.swift
@@ -23,6 +23,16 @@ let package = Package(
             name: "PolyNetSDK",
             url:"https://artifacts.s73cloud.com/repository/maven-s73-releases/s73-polynet-plat/polynet-swift-package-manager/5.2.0/polynet-swift-package-manager-5.2.0.zip",
             checksum: "9cb83606e31349d63703e583aee92c9a3c1028f534d700f66b40bb9b37ad70d6"
+        ),
+        .binaryTarget(
+            name: "SwiftProtobuf",
+            url:"https://artifacts.s73cloud.com/repository/maven-s73-releases/s73-polynet-plat/polynet-ios-swift-protobuf-framework/1.12.0/polynet-ios-swift-protobuf-framework-1.12.0.zip",
+            checksum: "6e169330972030e818e60f4fcb42c314c28d7ac00e215234abf8f0bea9f08910"
+        ),
+        .binaryTarget(
+            name: "Starscream",
+            url:"https://artifacts.s73cloud.com/repository/maven-s73-releases/s73-polynet-plat/polynet-ios-starscream-framework/3.1.1/polynet-ios-starscream-framework-3.1.1.zip",
+            checksum: "d946186b8fddf8acc21209ec826e00fe32655bb77839cd8ebcadc4211398a721"
         )
     ]
 )
