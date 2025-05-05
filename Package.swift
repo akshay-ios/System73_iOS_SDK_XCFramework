748f05f716e097001c7ccbef38e457e02e2a42b0
Akshay Kumar <akshaykumar@Akshays-MacBook-Air.local>
2025-05-05 16:59:35 +0530

    make changes in package class

--- Package.swift
+++ Package.swift
@@ -15,14 +15,14 @@ let package = Package(
     ],
     dependencies: [
         // dependency version match
-//        .package(url: "https://github.com/apple/swift-protobuf.git", exact: "1.26.0"),
-//        .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.8")
+        //        .package(url: "https://github.com/apple/swift-protobuf.git", exact: "1.26.0"),
+        //        .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.8")
     ],
     targets: [
         .binaryTarget(
             name: "PolyNetSDKBinary",
-            url:"https://artifacts.s73cloud.com/repository/maven-s73-releases/s73-polynet-plat/polynet-swift-package-manager/5.2.0/polynet-swift-package-manager-5.2.0.zip",
-            checksum: "9cb83606e31349d63703e583aee92c9a3c1028f534d700f66b40bb9b37ad70d6"
+            url:"https://artifacts.s73cloud.com/repository/maven-s73-releases/s73-polynet-plat/polynet-ios-swift-package-manager/5.2.0/polynet-ios-swift-package-manager-5.2.0.zip",
+            checksum: "2b1b191435795536153a3fc4ffaa4a2746ac86208d26a1610c7dc326e8c31a71"
         ),
         .target(
             name: "PolyNetSDK",
@@ -30,6 +30,7 @@ let package = Package(
                 "PolyNetSDKBinary"
             ]
         )
+        
     ]
 )
 
